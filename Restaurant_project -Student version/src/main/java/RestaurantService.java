8e41d19457546501ce57fd9345431e1ce60c6bb7
kkallapur <kkallapur.krishna@gmail.com>
2024-05-19 15:10:36 +0530

    refactor RestaurantService

--- Restaurant_project -Student version/src/main/java/RestaurantService.java
+++ Restaurant_project -Student version/src/main/java/RestaurantService.java
@@ -7,16 +7,13 @@ public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
 
     public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
-        Restaurant foundRestaurant = null;
         for (Restaurant restaurant : restaurants) {
-            if (restaurant.getName().toLowerCase().contains(restaurantName.toLowerCase())){
-                foundRestaurant = restaurant;
-                return foundRestaurant;
+            if (restaurant.getName().equalsIgnoreCase(restaurantName)){
+                return restaurant;
             }
         }
         throw new restaurantNotFoundException("Restaurant not found: " + restaurantName);
     }
-
 
     public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
         Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
